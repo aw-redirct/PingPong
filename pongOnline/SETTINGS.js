20ce219e54deb2756b92fb61fc627f15e5c79426
garbage-audit <38815702+garbage-audit@users.noreply.github.com>
2018-11-09 18:04:31 -0500

    Add files via upload

---
+++ pongOnline/SETTINGS.js
@@ -0,0 +1,43 @@
+
+
+var SETTINGS = {
+  X: alert("Enter Admin Password"),
+  VER : "1.0",
+  WIDTH : 1200,
+  HEIGHT : 800,
+  BORDER_WIDTH : 0,
+  BACKGROUND_COLOR : "#FFFFFF",
+  PLAYER : {
+    WIDTH : 20,
+    HEIGHT : x, //165
+    GAP : 25
+  },
+  BALL : {
+    WIDTH : 50,
+    HEIGHT : 50,
+  },
+  NET : {
+    WIDTH : 6
+  },
+  SCORE : {
+    Y : 40,
+    SIZE : 30,
+    GAP : 50
+  },
+  EDGE_SHOOT_ANGLE_ADJUST : 10,
+  EDGE_ANGLE : 6,
+  STRAIGHT_ADJUST : 7,
+  STRATGHT_ANGLE : 7,
+  SERVE_ANGLE : 15,
+  GOAL : 5
+};
+SETTINGS.CLIENT_SETTINGS = {
+  VER : SETTINGS.VER,
+  WIDTH : SETTINGS.WIDTH,
+  HEIGHT : SETTINGS.HEIGHT,
+  BORDER_WIDTH : SETTINGS.BORDER_WIDTH,
+  BACKGROUND_COLOR : SETTINGS.BACKGROUND_COLOR,
+  NET : SETTINGS.NET
+};
+
+module.exports = SETTINGS;
